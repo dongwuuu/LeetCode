dc3b6194613a27cc255db7f69b4a57700c8b3954
dongwuuu <572160979@qq.com>
2021-01-06 22:04:18 +0800

    update 399

---
+++ 399.除法求值.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode.cn id=399 lang=python3
+#
+# [399] 除法求值
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        from collections import defaultdict
+        graph = defaultdict(int)
+        set1 = set()
+        for i in range(len(equations)):
+            a, b = equations[i]
+            graph[(a, b)] = values[i]
+            graph[(b, a)] = 1/values[i]
+            set1.add(a)
+            set1.add(b)
+
+        # Floyd算法 求图中任意2点距离
+        arr = list(set1)
+        for k in arr:
+            for i in arr:
+                for j in arr:
+                    if graph[(i, k)] and graph[(k, j)]:
+                        graph[(i, j)] = graph[(i, k)] * graph[(k, j)]
+        
+        res = []
+        for x, y in queries:
+            if graph[(x, y)]:
+                res.append(graph[(x, y)])
+            else:
+                res.append(-1)
+        return res
+
+
+# equations = [["a","b"],["e","f"],["b","e"]]
+# values = [3.4,1.4,2.3]
+# queries = [["b","a"],["a","f"],["f","f"],["e","e"],["c","c"],["a","c"],["f","e"]]
+
+# print(Solution().calcEquation(equations, values, queries))
+
+# @lc code=end
+
