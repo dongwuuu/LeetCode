3394b68afb8f66ab8ff5e7d294d5927f004e9a84
dongwuuu <572160979@qq.com>
2021-02-06 21:22:27 +0800

    update 1423

---
+++ 1423.可获得的最大点数.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=1423 lang=python3
+#
+# [1423] 可获得的最大点数
+#
+
+# @lc code=start
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        # 滑动窗口大小为 n-k
+        windowSize = n - k
+        # 选前 n-k 个作为初始值
+        s = sum(cardPoints[:windowSize])
+        minSum = s
+        for i in range(windowSize, n):
+            # 滑动窗口每向右移动一格，增加从右侧进入窗口的元素值，并减少从左侧离开窗口的元素值
+            s += cardPoints[i] - cardPoints[i - windowSize]
+            minSum = min(minSum, s)
+        return sum(cardPoints) - minSum
+
+# @lc code=end
+
