874bf6326350ceaf1c60e5b3e52048bcd33c0b97
dongwuuu <572160979@qq.com>
2021-02-22 22:01:20 +0800

    update 766

---
+++ 766.托普利茨矩阵.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=766 lang=python3
+#
+# [766] 托普利茨矩阵
+#
+
+# @lc code=start
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(len(matrix) - 1):
+            if matrix[i][:-1] != matrix[i + 1][1:]:
+                return False
+        return True
+
+
+# @lc code=end
+
