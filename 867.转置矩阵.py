550aab113b8fe5a2f203a2db5799a3adffe6e6b4
dongwuuu <572160979@qq.com>
2021-02-25 12:53:13 +0800

    update 867

---
+++ 867.转置矩阵.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=867 lang=python3
+#
+# [867] 转置矩阵
+#
+
+# @lc code=start
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m, n = len(matrix), len(matrix[0])
+        transposed = [[0] * m for _ in range(n)]
+        for i in range(m):
+            for j in range(n):
+                transposed[j][i] = matrix[i][j]
+        return transposed
+
+
+# @lc code=end
+
