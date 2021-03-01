111326bf0cb97da6fa5b3aa13057d47049cc34d4
dongwuuu <572160979@qq.com>
2021-03-01 12:44:27 +0800

    update 303

---
+++ 303.区域和检索-数组不可变.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=303 lang=python3
+#
+# [303] 区域和检索 - 数组不可变
+#
+
+# @lc code=start
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.sums = [0]
+        _sums = self.sums
+
+        for num in nums:
+            _sums.append(_sums[-1] + num)
+
+    def sumRange(self, i: int, j: int) -> int:
+        _sums = self.sums
+        return _sums[j + 1] - _sums[i]
+
+
+# @lc code=end
+
