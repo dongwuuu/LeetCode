eb500742ea43c7ff33c1aac8ed97e6375822cb0f
dongwuuu <572160979@qq.com>
2021-03-03 23:10:40 +0800

    update 338

---
+++ 338.比特位计数.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=338 lang=python3
+#
+# [338] 比特位计数
+#
+
+# @lc code=start
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        def countOnes(x: int) -> int:
+            ones = 0
+            while x > 0:
+                x &= (x - 1)
+                ones += 1
+            return ones
+        
+        bits = [countOnes(i) for i in range(num + 1)]
+        return bits
+
+
+        
+# @lc code=end
+
